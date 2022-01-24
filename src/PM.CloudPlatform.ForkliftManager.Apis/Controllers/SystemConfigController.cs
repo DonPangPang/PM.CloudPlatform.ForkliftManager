566ae5bdaa153f7b161d99562136fe28b6700541
Pangpang Don <49866401+DonPangPang@users.noreply.github.com>
2022-01-24 18:09:36 +0800

    update

--- src/PM.CloudPlatform.ForkliftManager.Apis/Controllers/SystemConfigController.cs
+++ src/PM.CloudPlatform.ForkliftManager.Apis/Controllers/SystemConfigController.cs
@@ -1,9 +1,13 @@
-﻿using AutoMapper;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PM.CloudPlatform.ForkliftManager.Apis.Controllers.Base;
 using PM.CloudPlatform.ForkliftManager.Apis.Entities;
+using PM.CloudPlatform.ForkliftManager.Apis.General;
 using PM.CloudPlatform.ForkliftManager.Apis.Models;
 using PM.CloudPlatform.ForkliftManager.Apis.Repositories;
 
@@ -18,8 +22,30 @@ namespace PM.CloudPlatform.ForkliftManager.Apis.Controllers
     [Authorize]
     public class SystemConfigController : MyControllerBase<SystemConfigRepository, SystemConfig, SystemConfigDto, SystemConfigAddOrUpdateDto>
     {
-        public SystemConfigController(SystemConfigRepository repository, IMapper mapper) : base(repository, mapper)
+        private readonly IGeneralRepository generalRepository;
+
+        public SystemConfigController(SystemConfigRepository repository, IMapper mapper, IGeneralRepository generalRepository) : base(repository, mapper)
         {
+            this.generalRepository = generalRepository;
+        }
+
+        /// <summary>
+        /// 查询系统设置
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetSystemConfig()
+        {
+            var config = await generalRepository.GetQueryable<SystemConfig>()
+                .OrderByDescending(x=>x.CreateDate)
+                .FirstOrDefaultAsync();
+
+            if(config is null)
+            {
+                return Success(new SystemConfig());
+            }
+
+            return Success(config);
         }
     }
 }
