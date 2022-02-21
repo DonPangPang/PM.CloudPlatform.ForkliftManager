36dccbfa9d19bbc9b99f7057e923fe6458638cfe
Pangpang Don <49866401+DonPangPang@users.noreply.github.com>
2022-02-21 09:48:18 +0800

    Update ModuleController.cs

--- src/PM.CloudPlatform.ForkliftManager.Apis/Controllers/ModuleController.cs
+++ src/PM.CloudPlatform.ForkliftManager.Apis/Controllers/ModuleController.cs
@@ -2,8 +2,11 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PM.CloudPlatform.ForkliftManager.Apis.Controllers.Base;
 using PM.CloudPlatform.ForkliftManager.Apis.Entities;
+using PM.CloudPlatform.ForkliftManager.Apis.General;
+using PM.CloudPlatform.ForkliftManager.Apis.Helper;
 using PM.CloudPlatform.ForkliftManager.Apis.Models;
 using PM.CloudPlatform.ForkliftManager.Apis.Repositories;
 using PM.CloudPlatform.ForkliftManager.Apis.Repositories.Base;
@@ -19,8 +22,10 @@ namespace PM.CloudPlatform.ForkliftManager.Apis.Controllers
     [Authorize("Identify")]
     public class ModuleController : MyControllerBase<ModuleRepository, Module, ModuleDto, ModuleAddOrUpdateDto>
     {
-        public ModuleController(ModuleRepository repository, IMapper mapper) : base(repository, mapper)
+        public readonly IGeneralRepository _generalRepository;
+        public ModuleController(ModuleRepository repository, IMapper mapper, IGeneralRepository generalRepository) : base(repository, mapper)
         {
+            _generalRepository = generalRepository;
         }
     }
 }
